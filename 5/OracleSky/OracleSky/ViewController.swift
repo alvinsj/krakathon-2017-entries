8ffff4bd19fceb5f895e4329b34fc822d02b7d8b
alvinsj <text@alvinsj.net>
2017-10-21 16:55:54 +0400

    base commit

---
+++ 5/OracleSky/OracleSky/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OracleSky
+//
+//  Created by alvinsj on 21/10/17.
+//  Copyright © 2017 alvinsj.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var deg: UILabel!
+    @IBOutlet weak var place: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var status: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
